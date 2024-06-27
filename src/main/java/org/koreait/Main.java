880b9b6b6a4558839926f5131cfa786fbc8b8530
Yubeol <mingu30638@gmail.com>
2024-06-27 14:26:36 +0900

    24_Exam_RMK

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -5,15 +5,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        fibonacci(10);
+    }
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
+    public static void fibonacci(int n) {
+        int num1 = 1;
+        int num2 = 0;
+        int sum = 0;
+
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(sum + " ");
+
+            sum = num1 + num2;
+            num1 = num2 ;
+            num2 = sum;
+
+
         }
     }
 }
