a8bd81351f0507a9c3017482740d87ac54548e39
Yubeol <mingu30638@gmail.com>
2024-06-27 14:56:35 +0900

    24_Exam_RMK

--- src/main/java/org/koreait/Main.java
+++ src/main/java/org/koreait/Main.java
@@ -9,7 +9,7 @@ public class Main {
         int n = in.nextInt();
 
        for (int i = 0; i < n; i++) {
-           for (int j = 0; j <= 2 * i - 1; j++) {
+           for (int j = 0; j <= n; j++) {
                System.out.print(" ");
            }
            for (int j = n; j > i; j--) {
